232682e85e37d05739d31ff55d5f74873ffcac59
azuuuuhuuuuy <huytnd1910@gmail.com>
2026-06-19 00:16:35 +0700

    Update and rename TEST.js to script.user.js

---
+++ script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Tìm em, tìm em trong bóng đêm..
+// @namespace    https://github.com/azuzuzuzuzu/READING-MODE
+// @version      2.40-4
+// @description  hihibygpti, opus 4.8
+// @match        *://*/*
+// @run-at       document-body
+// @require      https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/scip.js
+// @updateURL    https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/scip.js
+// @downloadURL  https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/scip.js
+// @grant        GM_addStyle
+// ==/UserScript==
+console.log('working');
