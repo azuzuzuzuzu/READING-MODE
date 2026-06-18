0094eaf174142b801b98719a395af129fd1b5fa7
azuuuuhuuuuy <huytnd1910@gmail.com>
2026-06-19 00:38:47 +0700

    zdfzdfz

--- script.user.js
+++ script.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Tìm em, tìm em trong bóng đêm..
 // @namespace    https://github.com/azuzuzuzuzu/READING-MODE
-// @version      2.40-4
+// @version      2.41
 // @description  hihibygpti, opus 4.8
 // @match        *://*/*
 // @run-at       document-body
 // @grant        GM_addStyle
 // @updateURL    https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/main/script.user.js
 // @downloadURL  https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/main/script.user.js
-// @require      https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/script.user.js
 // ==/UserScript==
 console.log('loadin');
 (function () {
