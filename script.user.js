2a127868f23bbfe99a81bb6aac90f3aad332d6cd
azuuuuhuuuuy <huytnd1910@gmail.com>
2026-06-19 00:25:58 +0700

    huibhhu

--- script.user.js
+++ script.user.js
@@ -5,9 +5,9 @@
 // @description  hihibygpti, opus 4.8
 // @match        *://*/*
 // @run-at       document-body
-// @require      https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/scip.js
-// @updateURL    https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/scip.js
-// @downloadURL  https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/scip.js
 // @grant        GM_addStyle
+// @updateURL    https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/main/script.user.js
+// @require      https://raw.githubusercontent.com/azuzuzuzuzu/READING-MODE/refs/heads/main/script.user.js
 // ==/UserScript==
 console.log('working');
